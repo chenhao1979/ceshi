780efd5498a041a9296a28b1c04bcf804f66285a
TJBHB <dxm919@126.com>
2024-09-10 23:25:11 +0800

    1

---
+++ ceshi.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName ceshi  //类名称
+ * @Description: 类描述
+ * @Author: TJBHB    //作者
+ * @CreateDate: 2024/9/10 23:24	//创建时间
+ * @UpdateUser: 更新人
+ * @UpdateDate: 2024/9/10 23:24	//更新时间
+ * @UpdateRemark: 更新的信息
+ * @Version: 1.0    //版本号
+ */
+public class ceshi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,world");
+    }
+}
+
